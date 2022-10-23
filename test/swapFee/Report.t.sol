14397bfb90b27d6db76363c168d824e07daf646e
Shayan S <91401566+sha256yan@users.noreply.github.com>
2022-10-24 06:41:57 +1100

    report

--- test/swapFee/Report.t.sol
+++ test/swapFee/Report.t.sol
@@ -8,19 +8,56 @@ contract Report is SwapFeeTestHelper {
 
 
 
-    function _calculateFee(LBPair _pair, uint256 _amountIn) internal view returns(uint256 correctPairFees) {
-        FeeHelper.FeeParameters memory _fp = _pair.feeParameters();
-        (uint256 baseFee, uint256 variableFee) = (FeeHelper.getBaseFee(_fp), FeeHelper.getVariableFee(_fp));
-        uint256 totalFee = baseFee + variableFee;
-        uint256 correctPairFees = (_amountIn * totalFee) / Constants.PRECISION;
-        return correctPairFees;
+
+    function testCorrectFeeBinDeactivation() public {
+
+        uint112 _token6DAmountIn = 1e10;
+        uint256 correctPairFees = _calculateFee(correctFeePair, _token6DAmountIn);
+        uint16 numBins = 2;
+
+        //each bin will have more than enough liquidity for the swap. activeId should not change.
+        uint112 _token18DAmount = (_token6DAmountIn - uint112(correctPairFees) + 100) * numBins;
+
+        //give liquidity to 2 bins so we can test whether the activeId moves.
+        _createSpreadLiquidity(correctFeePair, _token18DAmount, numBins);
+        
+
+        uint256 pairFees = _getFeesFromSwap(correctFeePair, _token6DAmountIn);
+
+        (,,uint id) = correctFeePair.getReservesAndId();
+
+        emit log_named_uint("current id: ", id);
+        emit log_named_uint("init id:", 1 << 23);
+
+        //this is the correct behaviour
+        assertTrue(id == 1 << 23);
     }
 
-    function _calculateDeficitPercentage(uint _smallFee, uint _bigFee, uint _precision) internal pure returns(uint deficitPercentage) {
-        return((_bigFee -  _smallFee) * _precision) / _bigFee;
+
+
+    function testFalsePositiveBinDeactivation() public {
+
+        uint112 _token6DAmountIn = 1e10;
+        uint256 correctPairFees = _calculateFee(pair, _token6DAmountIn);
+        uint16 numBins = 2;
+
+        //each bin will have more than enough liquidity for the swap. activeId should not change, but it does.
+        uint112 _token18DAmount = (_token6DAmountIn - uint112(correctPairFees) + 100) * numBins;
+
+        //give liquidity to 2 bins so we can test whether the activeId moves.
+        _createSpreadLiquidity(pair, _token18DAmount, numBins);
+        
+
+        uint256 pairFees = _getFeesFromSwap(pair, _token6DAmountIn);
+
+        (,,uint id) = pair.getReservesAndId();
+
+        emit log_named_uint("current id: ", id);
+        emit log_named_uint("init id:", 1 << 23);
+
+        //this is incorrect behaviour. 
+        assertTrue(id != 1 << 23);
     }
-
-
 
 
 
@@ -31,10 +68,10 @@ contract Report is SwapFeeTestHelper {
         _createLiquidity(pair, _token18DAmount);
         uint112 _token6DAmountIn = 1e10;
         
-        /// @note verify _calculateFee above
+        /// @note verify _calculateFee below
         uint256 correctPairFees = _calculateFee(pair, _token6DAmountIn);
 
-        //record fees from actual swap. (uses )
+        //record fees from actual swap.
         uint256 pairFees = _getFeesFromSwap(pair, _token6DAmountIn);
 
         emit log_named_uint("fee deficit: ", correctPairFees - pairFees);
@@ -74,6 +111,23 @@ contract Report is SwapFeeTestHelper {
 
         emit log_string("MultiBin deficitPercentages: ");
         _arrayLog(deficitPercentages);
+    }
+
+
+
+
+
+    function _calculateFee(LBPair _pair, uint256 _amountIn) internal view returns(uint256 correctPairFees) {
+        FeeHelper.FeeParameters memory _fp = _pair.feeParameters();
+        (uint256 baseFee, uint256 variableFee) = (FeeHelper.getBaseFee(_fp), FeeHelper.getVariableFee(_fp));
+        uint256 totalFee = baseFee + variableFee;
+        uint256 correctPairFees = (_amountIn * totalFee) / Constants.PRECISION;
+        return correctPairFees;
+    }
+
+
+    function _calculateDeficitPercentage(uint _smallFee, uint _bigFee, uint _precision) internal pure returns(uint deficitPercentage) {
+        return((_bigFee -  _smallFee) * _precision) / _bigFee;
     }
 
 
